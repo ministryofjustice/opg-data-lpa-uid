5b4a35fb8ee4c6ee8ef8bb4ac933f8c35ff8a9d4
Greg Tyler <gregtyler@users.noreply.github.com>
2023-03-27 16:16:56 +0100

    VEGA-1761: Build local infrastructure for LPA ID service (#13)
    * VEGA-1761 - Initial local account & environment terraform #minor
    
    * Add hello world Lambda container
    
    Can be executed from localstack with:
    ```sh
    awslocal lambda invoke --endpoint http://lambda-create-case:8080 --function-name=function --payload='{"message":"something"}' /dev/stdout
    ```
    
    #minor
    
    * Add delegator to route lambda traffix
    
    Because we're not using localstack Pro, we need to handle the Lambda routing ourselves. Also, aws-lambda-rie causes a mismatch in the lambda name (it's always called "function") which our delegator can take care of.
    
    #minor
    
    * Add API Gateway
    
    Can be executed with:
    ```sh
    curl -XPOST http://{api-id}.execute-api.localhost.localstack.cloud:4566/current/cases -d '{some message}'
    ```
    
    #minor
    
    * Refactor delegator
    
    - Move out of `./lambda` directory to clarify it's not a lambda
    - Stop exposing a port
    - Add a `go.work` file for IDEs
    - Use consistent naming conventions for easier routing
    - Move policy to operation, so it's 1-1-1 between lambda/operation/policy
    - Tune lambda to more appropriate config
    
    #minor
    
    * VEGA-1761 - DNS + Dynamo infra #minor
    
    * VEGA-1761 - Switch terraform pattern to fix tflocal issues, and remove tflocal #minor
    
    ---------
    
    Co-authored-by: Andy Price <sixdaysandy@gmail.com>
    Co-authored-by: Aidan Johnson <aidan.johnson@digital.justice.gov.uk>

---
+++ terraform/modules/region/dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "lpa_uid" {
+  name             = "lpa-uid-${local.environment_name}"
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "uid"
+
+  deletion_protection_enabled = true
+
+  attribute {
+    name = "uid"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
