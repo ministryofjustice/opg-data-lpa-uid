1625868a0c6e8f4dc828f14ec613bd46f0965a8d
GemTay <gemma.taylor@digital.justice.gov.uk>
2023-03-31 12:02:09 +0100

    generated a UID with checksum as ending character which can be validated. Created tests to cover all functions. VEGA-1787

--- lambda/create-case/go.mod
+++ lambda/create-case/go.mod
@@ -3,3 +3,12 @@ module github.com/ministryofjustice/opg-data-lpa-uid/lambda/create-case
 go 1.20
 
 require github.com/aws/aws-lambda-go v1.38.0
+
+require (
+	github.com/calmh/luhn v2.0.0+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- lambda/create-case/uid_generator.go
+++ lambda/create-case/uid_generator.go
@@ -2,42 +2,79 @@ package main
 
 import (
 	"crypto/rand"
+	"math"
 	"math/big"
 	"strconv"
+	"strings"
 )
 
+var AllChars = []string{"3", "4", "6", "7", "8", "9", "Q", "W", "E", "R", "T", "Y", "U", "P", "A", "D", "F", "G", "H", "J", "K", "L", "X", "C", "V", "B", "N", "M"}
+var N = float64(len(AllChars))
+
 func generateUID() (string, error) {
-	allChars := []string{"3", "4", "6", "7", "8", "9", "Q", "W", "E", "R", "T", "Y", "U", "P", "A", "D", "F", "G", "H", "J", "K", "L", "X", "C", "V", "B", "N", "M"}
 	numChars := []string{"3", "4", "6", "7", "8", "9"}
 
-	var v int64
-	uid := "MTEST-"
+	uid := ""
 	lettersInSequence := 0
 
-	for i := 1; i <= 12; i++ {
-		if lettersInSequence == 2 {
-			index, err := rand.Int(rand.Reader, big.NewInt(int64(len(numChars))))
-			if err != nil {
-				return "", err
-			}
-			v = index.Int64()
-			lettersInSequence = 0
-		} else {
-			index, err := rand.Int(rand.Reader, big.NewInt(int64(len(allChars))))
-			if err != nil {
-				return "", err
-			}
-			v = index.Int64()
+	for x := 1; x <= 11; x++ {
+		max := len(AllChars)
 
-			if _, err := strconv.Atoi(allChars[v]); err != nil {
-				lettersInSequence++
-			} else {
-				lettersInSequence = 0
-			}
+		if lettersInSequence == 2 {
+			max = len(numChars)
 		}
 
-		uid = uid + allChars[v]
+		index, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+		if err != nil {
+			return "", err
+		}
+
+		i := index.Int64()
+		if _, err := strconv.Atoi(AllChars[i]); err != nil {
+			lettersInSequence++
+		} else {
+			lettersInSequence = 0
+		}
+
+		uid = uid + AllChars[i]
 	}
 
-	return uid, nil
+	checksum := generateChecksum(uid)
+
+	return "MTEST" + uid + checksum, nil
+}
+
+func generateChecksum(uid string) string {
+	sum := checksumCalculation(uid, 2)
+
+	remainder := math.Mod(sum, N)
+	checkCodePoint := math.Mod(N-remainder, N)
+
+	return AllChars[int(checkCodePoint)]
+}
+
+func validateChecksum(uid string) bool {
+	sum := checksumCalculation(uid, 1)
+	remainder := math.Mod(sum, N)
+	return remainder == 0
+}
+
+func checksumCalculation(uid string, factor int) float64 {
+	var sum = 0.00
+
+	for i := len(uid) - 1; i >= 0; i-- {
+		codePoint := strings.Index("346789QWERTYUPADFGHJKLXCVBNM", strings.Split(uid, "")[i])
+		var addend = float64(factor * codePoint)
+
+		if factor == 2 {
+			factor = 1
+		} else {
+			factor = 2
+		}
+
+		addend = math.Floor(addend/N) + math.Mod(addend, N)
+		sum = sum + addend
+	}
+
+	return sum
 }
