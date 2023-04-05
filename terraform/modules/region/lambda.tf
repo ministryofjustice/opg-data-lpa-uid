22551a62c05805921f4f5d4941c964010b779ed6
Gemma Taylor <gemma.taylor@digital.justice.gov.uk>
2023-04-05 09:14:30 +0100

    VEGA-1775 add multi region support for LPA UID service (#20)
    * VEGA-1775 - Make Multi Region #minor
    * Add DynamoDB KMS Encryption
    * Make DynamoDB Global Table with Replica
    * Add weighted DNS for each regional endpoint
    * Add local eu-west-2
    
    * VEGA-1775 - make dynamodb global #minor
    
    * VEGA-1775 - added regex to match delegator region in request path. Added make commands to get item and scan table. VEGA-1775 #minor"
    
    * VEGA-1775 - added eof lines
    
    ---------
    
    Co-authored-by: Andy Price <sixdaysandy@gmail.com>

--- terraform/modules/region/lambda.tf
+++ terraform/modules/region/lambda.tf
@@ -6,6 +6,12 @@ resource "aws_lambda_function" "create_case" {
   timeout       = 5
   memory_size   = 128
 
+  environment {
+    variables = {
+      AWS_DYNAMODB_TABLE_NAME = var.is_primary ? aws_dynamodb_table.lpa_uid[0].name : var.dynamodb_primary_name
+    }
+  }
+
   vpc_config {
     subnet_ids         = data.aws_subnets.private.ids
     security_group_ids = []
