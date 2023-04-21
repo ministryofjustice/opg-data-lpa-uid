3b077c5e5b57a43fd38f2acabb3401c6ea602033
GemTay <gemma.taylor@digital.justice.gov.uk>
2023-04-21 16:19:34 +0100

    changed stage name to fix tf error. VEGA-1789 #minor

--- terraform/modules/region/api_gateway.tf
+++ terraform/modules/region/api_gateway.tf
@@ -100,7 +100,7 @@ resource "aws_api_gateway_base_path_mapping" "mapping" {
 
 resource "aws_api_gateway_method_settings" "lpa_uid_gateway_settings" {
   rest_api_id = aws_api_gateway_rest_api.lpa_uid.id
-  stage_name  = aws_api_gateway_deployment.lpa_uid.stage_name
+  stage_name  = aws_api_gateway_stage.current.stage_name
   method_path = "*/*"
 
   settings {
