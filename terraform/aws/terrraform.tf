aac23ae9799669168091ca3257e059908d57d5d5
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2023-05-09 15:51:28 +0100

    Add deployment pipeline
    - Build and cache images
    - Separate unit and integration tests
    - Push image to ECR
    - Deploy to development
    
    For deployment I've used a pattern from opg-modernising-lpa which uses reusable workflows. I've also fleshed out the AWS workspace folder to create the region resources.
    
    Fixes VEGA-1739 #minor

--- terraform/aws/terrraform.tf
+++ terraform/aws/terrraform.tf
@@ -46,6 +46,20 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias  = "eu-west-2"
+  region = "eu-west-2"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::${local.environment.account_id}:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+provider "aws" {
   alias  = "management"
   region = "eu-west-1"
 
