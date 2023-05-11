eedafe25cba2cc844d31aadaa4fde0f9ed9703aa
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2023-05-11 11:32:28 +0100

    name_prefix -> name
    #patch

--- terraform/modules/region/vpc.tf
+++ terraform/modules/region/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "lambda" {
-  name_prefix = "lpa-uid-${local.environment_name}"
+  name        = "lpa-uid-${local.environment_name}"
   vpc_id      = data.aws_vpc.sirius.id
   description = "LPA UID Lambda security group"
 
