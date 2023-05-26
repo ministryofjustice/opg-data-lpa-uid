952ed39e8bca1938d0195ae96b8c60d941af0407
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-26 14:12:36 +0100

    put refactor file in the correct place

--- terraform/aws/refactor.tf
+++ terraform/aws/refactor.tf
@@ -1,3 +1,8 @@
+# moved {
+#   from = module.eu-west-2.aws_dynamodb_table_replica.lpa_uid[0]
+#   to   = module.global.aws_dynamodb_table.lpa_uid.replica
+# }
+
 moved {
   from = module.eu-west-1.aws_dynamodb_table.lpa_uid[0]
   to   = module.global.aws_dynamodb_table.lpa_uid
@@ -6,6 +11,11 @@ moved {
 moved {
   from = module.eu-west-1.aws_kms_alias.dynamodb_alias
   to   = module.global.aws_kms_alias.dynamodb_alias_eu_west_1
+}
+
+moved {
+  from = module.eu-west-2.aws_kms_alias.dynamodb_alias
+  to   = module.global.aws_kms_alias.dynamodb_alias_eu_west_2
 }
 moved {
   from = module.eu-west-1.aws_kms_key.dynamodb
