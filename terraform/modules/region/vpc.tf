cd789137ded2b4182aeee234a29bdb42db5a7a40
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-26 11:23:54 +0100

    allow the lambda any egress

--- terraform/modules/region/vpc.tf
+++ terraform/modules/region/vpc.tf
@@ -7,3 +7,12 @@ resource "aws_security_group" "lambda" {
     create_before_destroy = true
   }
 }
+resource "aws_security_group_rule" "lambda_egress" {
+  description       = "Allow any egress from lambda"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:aws-ec2-no-public-egress-sgr - open egress for load balancers
+  security_group_id = aws_security_group.lambda.id
+}
