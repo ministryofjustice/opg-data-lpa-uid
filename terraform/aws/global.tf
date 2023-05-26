1abb6040ced3c089f906a43e0614cb9e132a7aa6
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-26 14:29:55 +0100

    fix local and format

--- terraform/aws/global.tf
+++ terraform/aws/global.tf
@@ -4,7 +4,7 @@ module "global" {
   environment_name = local.environment_name
 
   providers = {
-    aws.global = aws.global
+    aws.global    = aws.global
     aws.eu-west-1 = aws.eu-west-1
     aws.eu-west-2 = aws.eu-west-2
   }
