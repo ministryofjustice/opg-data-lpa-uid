6027e3efc4a2f7ba532cd8712f21014c9420754c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-30 10:37:00 +0100

    Add is_local variable to turn off unsupported features for infra localstack

--- terraform/local/regions.tf
+++ terraform/local/regions.tf
@@ -1,37 +1,43 @@
-module "eu-west-1" {
-  source               = "../modules/region"
-  depends_on           = [module.local_setup]
-  app_version          = "latest"
-  dynamodb_arn         = module.global.dynamodb_table.arn
-  dynamodb_kms_key_arn = module.global.dynamodb_table.server_side_encryption[0].kms_key_arn
-  dynamodb_name        = module.global.dynamodb_table.name
-  environment_name     = local.environment_name
-  environment          = local.environment
-  is_local             = local.is_local
-  is_primary           = true
-  lambda_iam_role      = module.global.lambda_iam_role
+# module "eu-west-1" {
+#   source               = "../modules/region"
+#   depends_on           = [
+#     module.local_setup,
+#     module.global
+#     ]
+#   app_version          = "latest"
+#   dynamodb_arn         = module.global.dynamodb_table.arn
+#   dynamodb_kms_key_arn = module.global.dynamodb_table.server_side_encryption[0].kms_key_arn
+#   dynamodb_name        = module.global.dynamodb_table.name
+#   environment_name     = local.environment_name
+#   environment          = local.environment
+#   is_local             = local.is_local
+#   is_primary           = true
+#   lambda_iam_role      = module.global.lambda_iam_role
 
-  providers = {
-    aws            = aws.eu-west-1
-    aws.management = aws.management
-  }
-}
+#   providers = {
+#     aws            = aws.eu-west-1
+#     aws.management = aws.management
+#   }
+# }
 
-module "eu-west-2" {
-  source               = "../modules/region"
-  depends_on           = [module.local_setup]
-  app_version          = "latest"
-  dynamodb_arn         = [for o in module.global.dynamodb_table.replica : o][0].arn
-  dynamodb_kms_key_arn = [for o in module.global.dynamodb_table.replica : o][0].kms_key_arn
-  dynamodb_name        = module.global.dynamodb_table.name
-  environment_name     = local.environment_name
-  environment          = local.environment
-  is_local             = local.is_local
-  lambda_iam_role      = module.global.lambda_iam_role
+# module "eu-west-2" {
+#   source               = "../modules/region"
+#   depends_on           = [
+#     module.local_setup,
+#     module.global
+#     ]
+#   app_version          = "latest"
+#   dynamodb_arn         = [for o in module.global.dynamodb_table.replica : o][0].arn
+#   dynamodb_kms_key_arn = [for o in module.global.dynamodb_table.replica : o][0].kms_key_arn
+#   dynamodb_name        = module.global.dynamodb_table.name
+#   environment_name     = local.environment_name
+#   environment          = local.environment
+#   is_local             = local.is_local
+#   lambda_iam_role      = module.global.lambda_iam_role
 
-  providers = {
-    aws            = aws.eu-west-2
-    aws.management = aws.management
-  }
-}
+#   providers = {
+#     aws            = aws.eu-west-2
+#     aws.management = aws.management
+#   }
+# }
 
