3233e338a61e73d9a75818f154b79c6a714e82d8
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-30 10:52:38 +0100

    output eu-west-1 stage uri

--- terraform/local/outputs.tf
+++ terraform/local/outputs.tf
@@ -1,6 +1,6 @@
-# output "api_stage_uri_eu_west_1" {
-#   value = module.eu-west-1.api_stage_uri
-# }
+output "api_stage_uri_eu_west_1" {
+  value = module.eu-west-1.api_stage_uri
+}
 
 # output "api_stage_uri_eu_west_2" {
 #   value = module.eu-west-2.api_stage_uri
