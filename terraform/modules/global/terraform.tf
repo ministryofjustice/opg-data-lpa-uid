5ddf423ff46585552b2e7f745eef0eaa2a3bda2f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-30 16:06:19 +0100

    remove need to disamibuate name

--- terraform/modules/global/terraform.tf
+++ terraform/modules/global/terraform.tf
@@ -5,13 +5,8 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.59.0"
-      configuration_aliases = [
-        aws.global,
-      ]
     }
   }
 }
 
-data "aws_caller_identity" "current" {
-  provider = aws.global
-}
+data "aws_caller_identity" "current" {}
