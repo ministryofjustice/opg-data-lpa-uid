263ca05d4156b0af9c635e6cc224643b33a168b3
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-30 16:27:39 +0100

    restore previous setting

--- terraform/modules/region/iam.tf
+++ terraform/modules/region/iam.tf
@@ -1,7 +1,7 @@
 
 resource "aws_iam_role_policy" "lambda" {
   name   = "lpa-uid-lambda-${local.environment_name}"
-  role   = var.lambda_iam_role.name
+  role   = var.lambda_iam_role.id
   policy = data.aws_iam_policy_document.lambda.json
 }
 
