5beafbe5087b39b8352c376a7899edcd0fc97579
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-30 13:32:18 +0100

    pass replicas into region

--- terraform/modules/global/outputs.tf
+++ terraform/modules/global/outputs.tf
@@ -5,3 +5,7 @@ output "lambda_iam_role" {
 output "dynamodb_table" {
   value = aws_dynamodb_table.lpa_uid
 }
+
+output "dynamodb_table_replica" {
+  value = aws_dynamodb_table_replica.lpa_uid_eu_west_2
+}
