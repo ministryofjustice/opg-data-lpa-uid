3a480d2e3986c9fec6ef21507f3bbe397cff3291
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-30 16:03:40 +0100

    format local

--- terraform/local/regions.tf
+++ terraform/local/regions.tf
@@ -1,16 +1,16 @@
 module "eu-west-1" {
-  source               = "../modules/region"
-  depends_on           = [
+  source = "../modules/region"
+  depends_on = [
     module.local_setup,
     module.global
-    ]
-  app_version          = "latest"
+  ]
+  app_version               = "latest"
   dynamodb_global_table_arn = null
-  environment_name     = local.environment_name
-  environment          = local.environment
-  is_local             = true
-  is_primary           = true
-  lambda_iam_role      = module.global.lambda_iam_role
+  environment_name          = local.environment_name
+  environment               = local.environment
+  is_local                  = true
+  is_primary                = true
+  lambda_iam_role           = module.global.lambda_iam_role
 
   providers = {
     aws            = aws.eu-west-1
@@ -19,19 +19,19 @@ module "eu-west-1" {
 }
 
 module "eu-west-2" {
-  source               = "../modules/region"
-  depends_on           = [
+  source = "../modules/region"
+  depends_on = [
     module.local_setup,
     module.global
-    ]
-  app_version          = "latest"
+  ]
+  app_version               = "latest"
   dynamodb_global_table_arn = module.eu-west-1.dynamodb_table.arn
   # dynamodb_kms_key_arn = module.eu-west-1.dynamodb_table.kms_key_arn
-  environment_name     = local.environment_name
-  environment          = local.environment
-  is_local             = local.is_local
-  is_primary           = false
-  lambda_iam_role      = module.global.lambda_iam_role
+  environment_name = local.environment_name
+  environment      = local.environment
+  is_local         = local.is_local
+  is_primary       = false
+  lambda_iam_role  = module.global.lambda_iam_role
 
   providers = {
     aws            = aws.eu-west-2
