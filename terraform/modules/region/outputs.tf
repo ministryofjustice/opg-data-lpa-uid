79ad4acc12da629e3644f819e1dcf32b56db6778
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2023-05-31 10:36:31 +0100

    MLPAB-1028 - DynamoDB and KMS Permissions (#63)

--- terraform/modules/region/outputs.tf
+++ terraform/modules/region/outputs.tf
@@ -1,7 +1,3 @@
-output "dynamodb_arn" {
-  value = var.is_primary ? aws_dynamodb_table.lpa_uid[0].arn : ""
-}
-
-output "dynamodb_name" {
-  value = var.is_primary ? aws_dynamodb_table.lpa_uid[0].name : ""
+output "dynamodb_table" {
+  value = var.is_primary ? aws_dynamodb_table.lpa_uid[0] : aws_dynamodb_table_replica.lpa_uid[0]
 }
