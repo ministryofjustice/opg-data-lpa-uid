f8ad833dba80a2b30932a94ee7cd784eba251bef
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2023-11-23 16:47:40 +0000

    Upgrade to localstack v3
    Remove deprecated environment variables, and use forwarder lambda as in ministryofjustice/opg-s3-antivirus#341
    
    This leads to some slightly awkward conditionals on the lambda so that the local versions use the forwarder and the AWS-deployed versions use container images.
    
    Had to rename the API ID to `lpauid` to match AWS's regex expectations.
    
    Unfortunately, this leads to a hard-fork in the Lambda: in AWS environments it uses an image and locally it needs to use the zip-based forwarder to pass events to docker. Because these are contradictory properties in Terraform, we need to optional resources. I moved them into a module to simplify a bit.
    
    #minor

--- terraform/modules/region/lambda.tf
+++ terraform/modules/region/lambda.tf
@@ -1,11 +1,23 @@
-resource "aws_lambda_function" "create_case" {
-  function_name = "lpa-uid-create-case-${local.environment_name}"
-  image_uri     = "311462405659.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/integrations/lpa-uid-create-case-lambda:${var.app_version}"
-  package_type  = "Image"
-  role          = var.lambda_iam_role.arn
-  timeout       = 5
-  memory_size   = 128
+data "archive_file" "forwarder" {
+  count       = var.is_local ? 1 : 0
+  type        = "zip"
+  source_file = "${path.module}/../../../scripts/lambda/forwarder.py"
+  output_path = "${path.module}/../../../scripts/lambda/lambda.zip"
+}
 
+resource "aws_lambda_function" "create_case" {
+  function_name = var.is_local ? "lambda-create-case" : "lpa-uid-create-case-${local.environment_name}"
+  package_type  = var.is_local ? "Zip" : "Image"
+
+  image_uri = var.is_local ? null : "311462405659.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/integrations/lpa-uid-create-case-lambda:${var.app_version}"
+
+  filename = var.is_local ? data.archive_file.forwarder[0].output_path : null
+  handler  = var.is_local ? "forwarder.handler" : null
+  runtime  = var.is_local ? "python3.11" : null
+
+  role        = var.lambda_iam_role.arn
+  timeout     = 5
+  memory_size = 128
   environment {
     variables = {
       AWS_DYNAMODB_TABLE_NAME = var.is_primary ? aws_dynamodb_table.lpa_uid[0].name : split(":", aws_dynamodb_table_replica.lpa_uid[0].id)[0]
