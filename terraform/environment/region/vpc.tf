1be07b478704ffd74916da7fe269ee7b298ce213
Alex Saunders <acsauk@gmail.com>
2025-06-25 12:49:11 +0100

    emit metric event on stub creation

--- terraform/environment/region/vpc.tf
+++ terraform/environment/region/vpc.tf
@@ -13,7 +13,17 @@ resource "aws_security_group_rule" "lambda_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = data.aws_ip_ranges.dynamodb.cidr_blocks
+  cidr_blocks       = [data.aws_ip_ranges.dynamodb.cidr_blocks]
+  security_group_id = aws_security_group.lambda.id
+}
+
+resource "aws_security_group_rule" "lambda_egress_events" {
+  description       = "Allow Lambda to reach EventBridge VPC endpoint"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.sirius.cidr_block]
   security_group_id = aws_security_group.lambda.id
 }
 
