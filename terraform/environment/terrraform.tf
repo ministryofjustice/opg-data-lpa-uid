8ba18809422bf7e4ed82f639013f314ffe5345cb
Andy Price <sixdaysandy@gmail.com>
2026-03-30 14:33:52 +0100

    SP-3516 - Switch over workflows #minor

--- terraform/environment/terrraform.tf
+++ terraform/environment/terrraform.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket         = "opg.terraform.state"
-    key            = "opg-data-lpa-uid/terraform.tfstate"
-    encrypt        = true
-    region         = "eu-west-1"
-    dynamodb_table = "remote_lock"
     assume_role = {
-      role_arn = "arn:aws:iam::311462405659:role/integrations-ci"
+      role_arn = "arn:aws:iam::311462405659:role/opg-data-lpa-uid-terraform-state-access"
     }
+    bucket       = "opg.terraform.state"
+    encrypt      = true
+    key          = "opg-data-lpa-uid/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
   }
 
   required_providers {
